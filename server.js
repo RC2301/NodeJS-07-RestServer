3cf324c553ff0cb459d51c0490d8a2ea2810d2ee
RC2301 <richardcamacho66@hotmail.com>
2021-07-17 00:14:05 -0500

    Commit 4

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require("./server/config")
 const express = require("express");
 const app = express();
 
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/usuario', (req, res) => {
     res.json("get Usuario");
 });
+
 app.put('/usuario', function(req, res) {
     let body = req.body;
     if (body.nombre == undefined) {
@@ -23,9 +23,11 @@ app.put('/usuario', function(req, res) {
     }
 
 });
+
 app.post('/usuario', (req, res) => {
     res.json("post Usuario");
 });
+
 app.delete('/usuario', (req, res) => {
     res.json("delete Usuario");
 });
